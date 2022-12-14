4a4aa86cbe121e2188204fe077caa2bd7e72f888
Kyle Dudley <115148501+kewlkyle@users.noreply.github.com>
2022-12-14 09:55:43 +0100

    initial commit

---
+++ api.py
@@ -0,0 +1,129 @@
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import cross_origin
+import os
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/testdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Student(db.Model):
+    __tablename__ = 'student'
+    studentid = db.Column(db.Integer, primary_key=True)
+    studentfirstname = db.Column(db.String(80), unique=True, nullable=False)
+    studentlastname = db.Column(db.String(120), nullable=False)
+    studentadd = db.Column(db.String(120), nullable=False)
+    studentcity = db.Column(db.String(120), nullable=False)
+    studentstate = db.Column(db.String(120), nullable=False)
+    studentemail = db.Column(db.String(120), nullable=False)
+    bacourse= db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return "<Student %r>" % self.studentfirstname,self.studentlastname
+
+  
+@cross_origin()
+@app.route('/')
+def index():
+    return jsonify({"Welcome my PostgreSQL/Flask API"})
+
+@cross_origin()
+@app.route('/addstudent', methods = ['POST'])
+def create_student():
+    student_data = request.json
+
+    studentfirstname = student_data['studentfirstname']
+    studentlastname = student_data['studentlastname']
+    studentadd = student_data['studentadd']
+    studentcity = student_data['studentcity']
+    studentstate= student_data['studentstate']
+    studentemail = student_data['studentemail']
+    bacourse = student_data['bacourse']
+
+    #pet_description = pet_data['pet_description']
+    student = Student(
+                  studentfirstname =studentfirstname ,
+                  studentlastname = studentlastname, 
+                  studentadd = studentadd, 
+                  studentcity =studentcity,
+                  studentstate= studentstate,
+                  studentemail = studentemail,
+                  bacourse = bacourse)
+
+    db.session.add(student)
+    db.session.commit()
+    
+
+    return jsonify({"success": True,"response":"Pet added"})
+
+
+@cross_origin()    
+@app.route('/getstudent/<studentid>', methods = ['GET'])
+def getstudent(studentid):
+    student = Student.query.get(studentid)
+    del student.__dict__['_sa_instance_state']
+    return jsonify(student.__dict__)
+
+@cross_origin()    
+@app.route('/getstudents', methods = ['GET'])
+def getstudents():
+     all_students = []
+     students = Student.query.all()
+     print(type(students))
+     for student in students:
+          results = {
+                    "studentid":student.studentid,
+                    "studentfirstname":student.studentfirstname,
+                    "studentlastname":student.studentlastname,
+                    "studentadd":student.studentadd,
+                    "studentcity":student.studentcity,
+                    "studentstate":student.studentstate,
+                    "studentemail":student.studentemail,
+                    "bacourse":student.bacourse, }
+          all_students.append(results)
+
+     return jsonify(
+            {
+                "success": True,
+                "students": all_students,
+                "total_students": len(students),
+            }
+        )
+
+
+@app.route("/deletestudent/<int:student_id>", methods = ["DELETE"])
+def delete_student(studentid):
+    student = Student.query.get(studentid)
+    if student is None:
+        abort(404)
+    else:
+        db.session.delete(student)
+        db.session.commit()
+        return jsonify({"success": True, "response": "Student deleted"})
+        
+
+    
+@app.route("/updatestudent/<int:studentid>", methods = ["PATCH"])
+def update_student(studentid):
+    student = Student.query.get(studentid)
+    studentcity = request.json['studentcity']
+    studentstate = request.json['studentstate']
+
+    if student is None:
+        abort(404)
+    else:
+        student.studentcity = studentcity
+        student.studentstate = studentstate
+        db.session.add(student)
+        db.session.commit()
+        return jsonify({"success": True, "response": "Pet Details updated"})
+        
+
+
+
+db.create_all()
+if __name__ == '__main__':
+    app.run(debug=True,port=4000)
---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:flexywitdatech1@localhost/testdb'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = 'hi'
+
+db = SQLAlchemy(app)
+
+
+class Student(db.Model):
+    studentid = db.Column(db.Integer, primary_key=True)
+    studentfirstname = db.Column(db.String(80), unique=True, nullable=False)
+    studentlastname = db.Column(db.String(120), nullable=False)
+    studentadd = db.Column(db.String(120), nullable=False)
+    studentcity = db.Column(db.String(120), nullable=False)
+    studentstate = db.Column(db.String(120), nullable=False)
+    studentemail = db.Column(db.String(120), nullable=False)
+    bacourse= db.Column(db.String(120), nullable=False)
+
+    def __init__(self, studentid, studentfirstname, studentlastname, studentadd, studentcity,studentstate, studentemail, bacourse ):
+        self.studentid = studentid
+        self.studentfirstname = studentfirstname
+        self.studentlastname = studentlastname
+        self.studentadd = studentadd
+        self.studentcity = studentcity
+        self.studentstate = studentstate
+        self.studentemail = studentemail
+        self.bacourse = bacourse
+
+
+@app.route('/')
+def home():
+    return '<a href="/addperson"><button> Click here </button></a>'
+
+
+
+@app.route("/addperson")
+def addperson():
+    return render_template("index.html")
+
+
+@app.route("/personadd", methods=['POST'])
+def personadd():
+    studentid = request.form["studentid"]
+    studentfirstname = request.form["studentfirstname"]
+    studentlastname = request.form["studentlastname"]
+    studentadd = request.form["studentadd"]
+    studentcity = request.form["studentcity"]
+    studentstate = request.form["studentstate"]
+    studentemail = request.form["studentemail"]
+    bacourse = request.form["bacourse"]
+    entry = Student(studentid,studentfirstname, studentlastname, studentadd, studentcity,studentstate, studentemail, bacourse )
+    db.session.add(entry)
+    db.session.commit()
+
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run()
